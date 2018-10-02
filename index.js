8e32c1a56e7c40dd7aadd43055f456b38dd5fc65
Jayon Kim <jpkim921@gmail.com>
2018-10-02 19:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function Driver(name, age, experience){
   this.age = age
   this.experience = modexperienceel
 }
+
+function PickupLocation(address, city){
+  this.address = address
+  this.city = city
+}
